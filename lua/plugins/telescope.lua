e06087bcca7aca2c0786a6d77b8bc0a0216f026c
jubiman <bobvries111@gmail.com>
2025-10-25 15:44:45 +0200

    initial commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+   "nvim-telescope/telescope.nvim",
+   tag = "0.1.8",
+   dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
+      --[[{
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+    },]]
+   },
+}
